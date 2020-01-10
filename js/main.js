d0cac4020c35f2d572d25a74cd0f9e45ad903298
see-mik <1grazymike@gmail.com>
2020-01-10 17:50:10 +0200

    start code

--- js/main.js
+++ js/main.js
@@ -12,8 +12,26 @@ var Game = {
     start: function() {
         var canvas = document.getElementById("cvs");
         this.ctx = canvas.getContext("2d");
+        console.log('statt')
 
-        console.log("start");
+
+        this.imgs.bg = new Image();
+        this.imgs.bg.src = "img/bg.png";
+
+        this.run();
+    },
+
+    render: function() {
+        this.ctx.drawImage(this.imgs.bg, 0, 0);
+    },
+
+
+    run: function() {
+        this.render();
+
+        window.requestAnimationFrame(function() {
+            Game.run();
+        });
     }
 };
 
